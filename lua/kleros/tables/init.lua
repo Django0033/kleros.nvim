dc7076fe217bca5a3041da0e14e319b9fc782ccb
Eleazar Pequeno <edpu0033@gmail.com>
2026-04-11 14:46:10 -0500

    feat: add Ironsworn action table

--- lua/kleros/tables/init.lua
+++ lua/kleros/tables/init.lua
@@ -2,5 +2,6 @@ local M = {}
 
 M.test_table = require("kleros.tables.test_table")
 M.test_range_table = require("kleros.tables.test_range_table")
+M.ironsworn_action = require("kleros.tables.ironsworn_action")
 
 return M
